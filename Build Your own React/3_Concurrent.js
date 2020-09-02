bf8195ec0f10c57fa133c649ab7b3ec51dbb2972
aTmb405 <aTmb405@gmail.com>
2020-09-02 13:21:51 -0500

    Add notes for concurrent mode

--- Build Your own React/3_Concurrent.js
+++ Build Your own React/3_Concurrent.js
@@ -111,20 +111,20 @@ function render(element, container) {
 let nextUnitOfWork = null
 ​
 function workLoop(deadline) {
-  let shouldYield = false
-  while (nextUnitOfWork && !shouldYield) {
-    nextUnitOfWork = performUnitOfWork(
-      nextUnitOfWork
-    )
-    shouldYield = deadline.timeRemaining() < 1
-  }
-  requestIdleCallback(workLoop)
+    let shouldYield = false
+    while (nextUnitOfWork && !shouldYield) {
+        nextUnitOfWork = performUnitOfWork(
+            nextUnitOfWork
+        )
+        shouldYield = deadline.timeRemaining() < 1
+    }
+    requestIdleCallback(workLoop)
 }
 ​
 requestIdleCallback(workLoop)
 ​
 function performUnitOfWork(nextUnitOfWork) {
-  // TODO
+    // TODO
 }
   ​
 const Didact = {
